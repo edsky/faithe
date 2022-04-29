424e34bb7c38f31b42dc028b25fdaf45ebb1ff40
sy1ntexx <sy1ntexx@gmail.com>
2022-04-29 14:53:23 +0300

    Added support for lifetimes in interfaces

--- src/macros/interface.rs
+++ src/macros/interface.rs
@@ -1,4 +1,5 @@
 /// Creates an trait that emulates virtual table behavior from C++.
+/// You can use `'this` lifetime for arguments that requires `self` lifetime.
 /// ```
 /// # use faithe::interface;
 /// struct CPlayer;
@@ -6,6 +7,8 @@
 ///     trait IEntity(CPlayer) {
 ///         // 1 - is an index of this function in the table.
 ///         extern "C" fn print() = 1;
+///         // Lifetimes works too
+///         extern "C" fn other<'a>(num: &'a i32) -> &'a i32 = 2;
 ///     }
 /// }
 /// ```
@@ -15,7 +18,7 @@ macro_rules! interface {
         $(
             $vs:vis trait $name:ident$(($($target:ident),*))? {
                 $(
-                    extern $cc:tt fn $fn_id:ident($($arg_id:ident: $arg_ty:ty),*) $(-> $ret_ty:ty)? = $idx:expr;
+                    extern $cc:tt fn $fn_id:ident$(<$($gen:lifetime),*>)?($($arg_id:ident: $arg_ty:ty),*) $(-> $ret_ty:ty)? = $idx:expr;
                 )*
             }
         )*
@@ -25,7 +28,7 @@ macro_rules! interface {
                 $(
                     #[inline(always)]
                     #[allow(non_snake_case)]
-                    extern $cc fn $fn_id(&self, $($arg_id: $arg_ty),*) $(-> $ret_ty)? {
+                    extern $cc fn $fn_id<'this$(,$($gen),*)?>(&'this self, $($arg_id: $arg_ty),*) $(-> $ret_ty)? {
                         unsafe {
                             let slot = *(self as *const Self as *const usize) + $idx * core::mem::size_of::<usize>();
                             (*core::mem::transmute::<_, *const extern $cc fn(&Self, $($arg_ty),*) $(-> $ret_ty)?>(slot))
