6ab52de8661b1dc1289ce847935d20cecd86aa1f
sy1ntexx <sy1ntexx@gmail.com>
2022-04-29 19:13:41 +0300

    Made strptr, wideptr non null

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -18,10 +18,12 @@ cfg_if::cfg_if! {
 mod entry;
 pub use entry::*;
 
+use core::ptr::NonNull;
+
 /// Zero terminated ascii string. Have the same layout as `*const u8`.
 /// Used for convenience so it can be returned from extern funcs.
 #[repr(transparent)]
-pub struct StrPtr(*const u8);
+pub struct StrPtr(NonNull<u8>);
 
 impl StrPtr {
     /// Converts pointer into string slice.
@@ -29,7 +31,7 @@ impl StrPtr {
     /// `StrPtr` must point to valid memory.
     #[inline]
     pub unsafe fn to_str<'a>(&self) -> &'a str {
-        std::str::from_utf8_unchecked(crate::terminated_array(self.0, &0))
+        std::str::from_utf8_unchecked(crate::terminated_array(self.0.as_ptr(), &0))
     }
 
     /// Converts pointer to string by cloning data.
@@ -40,16 +42,18 @@ impl StrPtr {
         self.to_str().into()
     }
 
-    /// Creates new [`StrPtr`] from pointer.
+    /// Creates new [`StrPtr`] from pointer without checking if it's null.
+    /// # Safety
+    /// `p` must not be `null`.
     #[inline(always)]
-    pub const fn new(p: *const u8) -> Self {
-        Self(p)
+    pub const unsafe fn new_unchecked(p: *const u8) -> Self {
+        Self(NonNull::new_unchecked(p as _))
     }
 
-    /// Creates [`StrPtr`] pointing to `0`.
+    /// Creates new [`StrPtr`] from pointer.
     #[inline(always)]
-    pub const fn null() -> Self {
-        Self(core::ptr::null())
+    pub fn new(p: *const u8) -> Option<Self> {
+        Some(Self(NonNull::new(p as _)?))
     }
 }
 
@@ -63,25 +67,28 @@ macro_rules! str_ptr {
 
 /// Zero terminated UTF-16 string. Have the same layout as `*const u16`.
 /// Used for convenience so it can be returned from extern funcs.
-pub struct WidePtr(*const u16);
+#[repr(transparent)]
+pub struct WidePtr(NonNull<u16>);
 impl WidePtr {
     /// Converts pointer to string by cloning data.
     /// # Safety
     /// `WidePtr` must point to valid memory.
     #[inline]
     pub unsafe fn into_string(self) -> String {
-        String::from_utf16_lossy(crate::terminated_array(self.0, &0))
+        String::from_utf16_lossy(crate::terminated_array(self.0.as_ptr(), &0))
+    }
+
+    /// Creates new [`WidePtr`] from pointer without checking if it's null.
+    /// # Safety
+    /// `p` must not be `null`.
+    #[inline(always)]
+    pub const unsafe fn new_unchecked(p: *const u16) -> Self {
+        Self(NonNull::new_unchecked(p as _))
     }
 
     /// Creates new [`WidePtr`] from pointer.
     #[inline(always)]
-    pub fn new(p: *const u16) -> Self {
-        Self(p)
-    }
-
-    /// Creates [`WidePtr`] pointing to `0`.
-    #[inline(always)]
-    pub const fn null() -> Self {
-        Self(core::ptr::null())
+    pub fn new(p: *const u16) -> Option<Self> {
+        Some(Self(NonNull::new(p as _)?))
     }
 }
